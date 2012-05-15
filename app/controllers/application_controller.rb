ffd5a304ed29a97ae4031460dc99a0f02400395a
Guilherme Ceolin <guiceolin@gmail.com>
2012-05-14 23:30:59 -0300

    Login system

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,31 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :require_login
+
+  def current_user
+    @current_user ||= login_from_session
+  end
+
+  def login (user)
+    session[:user_id] = user.id
+    @current_user = user
+  end
+
+  def logout
+    session[:user_id] = nil
+    @current_user = nil
+  end
+
+  helper_method :current_user
+  private
+
+  def require_login
+    redirect_to new_session_path unless current_user
+  end
+
+  def login_from_session
+    User.find_by_id(session[:user_id]) if session[:user_id] || false
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,36 @@
 class User < ActiveRecord::Base
-  attr_accessible :crypted_password, :email, :salt, :username
+  attr_accessible :email, :username
+  attr_accessor :password
+  attr_protected :crypted_password, :salt
+
+  validates :password, confirmation: true, presence: { on: :create }
+  validates :email, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
+
+  before_save :encrypt_password
+
+
+  def self.authenticate(email,password)
+    user = find_by_email(email)
+    if user && user.crypted_password == user.encrypt(password, user.salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt(password, salt)
+    BCrypt::Engine.hash_secret(password, salt)
+  end
+
+
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.salt = BCrypt::Engine.generate_salt
+      self.crypted_password = encrypt(password, salt)
+    end
+  end
 end
