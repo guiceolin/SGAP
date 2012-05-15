83fcea61594aaf808546adca682624a10a708c4b
Guilherme Ceolin <guiceolin@gmail.com>
2012-05-14 21:01:37 -0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SGAP::Application.config.secret_token = '410659f2d4a90e5337cca130f8d5d026040f3a14d08daa2f512232565b1a1d42f3361b296bb088a8cf47ed1faf213b41ebccda2f4426c354bfe501ea79aeb846'
