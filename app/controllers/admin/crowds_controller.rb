a30a105798fe1cb468923fe2f91e3673390cb183
Guilherme Ceolin <guiceolin@gmail.com>
2012-07-11 00:11:50 -0300

    Layout + destroy actions

--- app/controllers/admin/crowds_controller.rb
+++ app/controllers/admin/crowds_controller.rb
@@ -1,6 +1,12 @@
 class Admin::CrowdsController < ApplicationController
 
   respond_to :html, :json
+
+  def search
+    @crowds = Crowd.search(params[:terms])
+    respond_with :admin, @crowds, layout: !request.xhr?
+  end
+
   def index
     @crowds = Crowd.all
     respond_with :admin, @crowds
@@ -30,6 +36,12 @@ class Admin::CrowdsController < ApplicationController
 
   def show
     @crowd = Crowd.find_by_id(params[:id])
+    respond_with :admin, @crowd
+  end
+
+  def destroy
+    @crowd = Crowd.find_by_id(params[:id])
+    @crowd.destroy
     respond_with :admin, @crowd
   end
 end
--- app/controllers/admin/enrollments_controller.rb
+++ app/controllers/admin/enrollments_controller.rb
@@ -3,7 +3,7 @@ class Admin::EnrollmentsController < ApplicationController
   def create
     @enrollment = Enrollment.new(params[:enrollment])
     if @enrollment.save
-      head :created, :location => crowd_enrollment_path(@enrollment.crowd,@enrollment)
+      head :created, :location => admin_crowd_enrollment_path(@enrollment.crowd,@enrollment)
     else
       head :unprocessable_entity
     end
@@ -11,5 +11,10 @@ class Admin::EnrollmentsController < ApplicationController
   def show
     @enrollment = Enrollment.find_by_id(params[:id])
     respond_with(@enrollment)
+  end
+  def destroy
+    @enrollment = Enrollment.find_by_id(params[:id])
+    @enrollment.destroy
+    redirect_to admin_crowd_path(params[:crowd_id])
   end
 end
