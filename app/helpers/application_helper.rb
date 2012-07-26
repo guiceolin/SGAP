5cba1b4fbb8c56280cc90661adebef3c02bae30a
Guilherme Ceolin <guiceolin@gmail.com>
2012-07-25 21:28:30 -0300

    first breadcrumb implementations
    will refactor it after i take a bath

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def breadcrumb(path)
+    all_paths = path.split('/').inject([]) do |memo,new|
+      if memo.blank?
+        memo << [new, new]
+      else
+        memo << [new, [memo.last.last,new].join('/')]
+      end
+      memo
+    end
+    # removes empty string that split create because first slash
+    all_paths.shift
+
+    breadcrumb_string = content_tag('ul', class: 'breadcrumb') do
+      all_paths.collect do |crumb|
+        content_tag(:li) do
+          link_to(crumb.first.capitalize, crumb.last) + content_tag(:span, '/', class: 'divider')
+        end
+      end.join('').html_safe
+    end
+    breadcrumb_string.html_safe
+  end
 end
