643801edf277792c9f351c23488a02ac3bf326f9
Guilherme Ceolin <guiceolin@gmail.com>
2012-08-22 22:51:36 -0300

    Subjects for professors

---
+++ app/controllers/professor/subjects_controller.rb
@@ -0,0 +1,16 @@
+class Professor::SubjectsController < ApplicationController
+  respond_to :html, :json
+
+  def index
+    @subjects = current_user.subjects
+    respond_with :admin, @subjects
+  end
+  def search
+    @subjects = current_user.subjects.search(params[:terms].present? ? params[:terms] : params[:term])
+    respond_with :admin, @subjects, layout: !request.xhr?
+  end
+  def show
+    @subject = current_user.subjects.find_by_code(params[:id])
+    respond_with :admin, @subject
+  end
+end
