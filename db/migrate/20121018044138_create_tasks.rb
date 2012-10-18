082ee02059d4df5877b14b34d1a85e7c7928b4e3
Guilherme Ceolin <guiceolin@gmail.com>
2012-10-18 02:40:12 -0300

    Task Migration

---
+++ db/migrate/20121018044138_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.date :start_date
+      t.date :end_date
+      t.references :solution
+      t.string :description
+      t.timestamps
+    end
+  end
+end
