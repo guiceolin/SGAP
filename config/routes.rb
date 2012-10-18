991ed17bb4afb3b151505e856864fe2518541f75
Guilherme Ceolin <guiceolin@gmail.com>
2012-10-18 02:41:23 -0300

    routing tasks

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,11 @@ SGAP::Application.routes.draw do
   end
   namespace :students do
     resources :crowds, only: [:show, :index] do
-      resources :enunciations, only: [:show]
+      resources :enunciations, only: [:show] do
+        resource :solution do
+          resources :tasks
+        end
+      end
     end
   end
   namespace :admin do
