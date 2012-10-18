644b51129803364347a9345c4c28980df37e5481
Guilherme Ceolin <guiceolin@gmail.com>
2012-10-18 01:37:34 -0300

    getting solution for an enunciation

--- app/controllers/students/enunciations_controller.rb
+++ app/controllers/students/enunciations_controller.rb
@@ -3,7 +3,7 @@ class Students::EnunciationsController < ApplicationController
 
   def show
     @enunciation = current_user.crowds.find_by_name(params[:crowd_id]).enunciations.find_by_name(params[:id])
+    @solution = current_user.groups.where(:enunciation_id => @enunciation.id).first.solution
     respond_with :students, @enunciation.crowd, @enunciation
   end
-
 end
