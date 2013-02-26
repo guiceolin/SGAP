fb41999228f6599303ac363828911acb78c65cbb
Guilherme Ceolin <guiceolin@gmail.com>
2013-02-26 19:21:53 -0300

    Using credentials from YML file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 OmniAuth.config.full_host = "http://localhost:3000"
 
+credentials = YAML.load(File.open(Rails.root + "config/google_credentials.yml"))
+
+::G_AUTH = credentials['key']
+::G_PASS =credentials['password']
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '525802749269.apps.googleusercontent.com', '40cwHuiNY2Io8jfI2G307MEa', :scope => 'userinfo.email,userinfo.profile,calendar'
+    provider :google_oauth2, G_AUTH, G_PASS, :scope => 'userinfo.email,userinfo.profile,calendar'
 end
